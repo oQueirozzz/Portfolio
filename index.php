fdb016c68d9b92be552fd9a26f5029dd1d85d1f7
oQueirozzz <renanqsrodrigues@gmail.com>
2025-01-24 11:59:21 -0300

    home feito

--- index.php
+++ index.php
@@ -1,112 +1,79 @@
-<php
-session_start();
-
-?>
 <!DOCTYPE html>
 <html lang="pt-br">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css">
-    <title>Portifólio</title>
+    <link rel="stylesheet" href="css/home.css">
+    <title>Document</title>
 </head>
 
 <body>
     <header>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid" >
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavDropdown" >
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="">Habilidades</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">características</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="projetos/projetos.php">Portfólio</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Contato</a>
-        </li>
-        
-        <li class="nav-item dropdown" id="dropdown-language">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Português
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Português</a></li>
-            <li><a class="dropdown-item" href="#">Inglês</a></li>
-            <li><a class="dropdown-item" href="#">Espanhol</a></li>
-          </ul>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+        <nav class="social">
+            <ul>
+                <li><a href="https://www.linkedin.com/in/renan-queiroz-a2a844338/" target="_blank" >Linkedin <img src="imgs/367593_linkedin_business_network_social_icon.png" alt=""></a></li>
+                <li><a href="#" target="_blank">Instagram <img src="imgs/1161953_instagram_icon.png" alt=""></a></li>
+                <li><a href="https://github.com/oQueirozzz" target="_blank">GitHub <img src="imgs/1378045_circle_git_github_hovytech_media_icon.png" alt=""></a></li>
+                <li><a href="#" target="_blank">Whatsapp <img src="imgs/5305166_call_chat_mobile_whatsapp_whatsapp logo_icon.png" alt=""> </a></li>
+            </ul>
+        </nav>
+
 
     </header>
-     
     <main>
-      <div class="social">
-        <ul>
-            <li><a href="#">Instagram <i class="imgs/icons8-instagram.gif"></i></a></li>
-            <li><a href="#">Linkedin <i class="fa fa-facebook"></i></a></li>
-            <li><a href="#">Github <i class="fa fa-dribbble"></i></a></li>
-            <li><a href="#">Whatsapp <i class="fa fa-behance"></i></a></li>
-        </ul>
-      </div>
-        
-        <section class="wall">
-
-          <div class="container-geral">
-              <div class="description">
-                <img src="img/OIP.jfif" alt="" width="48px" height="48px">
-                  <div class="name">
-                    <h1>Renan Queiroz</h1>
-                    <p>@oqueiroz._</p>
-                  </div>
+        <div class="container">
+            <div class="all">
+                <div class="lefter"><a href=""></a>
+               
+                    <div class="text">Experiências</div>
                 </div>
-              <div class="text-content">
-                    <p>
-                    Aspirante a desenvolvedor full stack, com foco em soluções eficientes e inovadoras. Confira meus projetos e portfólios que destacam minha experiência em desenvolvimento web, aplicativos e sistemas personalizados.
-                    </p>
-                  </div>
-              <div></div>
-          
-          </div>
-        </section>
+                <div class="left"> <a href="projetos/projetos.php"></a>
+                    <div class="text">Projetos</div>
+                </div>
+                <div class="center">
 
-        
-          
+                    <div class="explainer"><span>
+                            <div class="tooltip-container">
+                                <div class="relative">
+                                    <div class="group peer relative z-10 p-1">
+                                        <svg
+                                            class="duration-500 group-hover:rotate-[360deg] group-hover:scale-110"
+                                            height="90"
+                                            width="90"
+                                            viewBox="0 0 24 24"
+                                            xmlns="http://www.w3.org/2000/svg">
+                                            <path
+                                                d="M12 0C5.373 0 0 5.373 0 12s5.373 12 12 12 12-5.373 12-12S18.627 0 12 0zm0 22c-5.518 0-10-4.482-10-10s4.482-10 10-10 10 4.482 10 10-4.482 10-10 10zm-1-16h2v6h-2zm0 8h2v2h-2z"></path>
+                                        </svg>
+                                    </div>
+
+                                </div>
+                            </div>
+
+                            <!-- <i class="fa fa-search" aria-hidden="true"></i> -->
+                            <!-- <i class="fa fa-ellipsis-v" aria-hidden="true"></i> -->
+                            <!-- <img src="img/6936487_click_computer_mouse_mousepad_icon.png"  alt=""> -->
+                        </span>
+
+                    </div>
+                    <div class="text"></div>
+                </div>
+                <div class="right"> <a href=""></a>
+                    <div class="text">Minhas Habilidades</div>
+                </div>
+                <div class="righter"> <a href=""></a>
+                    <div class="text">A propósito</div>
+                </div>
+            </div>
+        </div>
     </main>
-<svg class="svg-curve" viewBox="0 0 1440 79" xmlns="http://www.w3.org/2000/svg">
-          <path d="M-100 0C-100 0 218.416 55.835 693.5 55.835C1168.58 55.835 1487 0 1487 0V79H-100V0Z"></path>
-        </svg>
     <footer>
-        
-        </footer>
-        
-        
-        <link href="//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
-        <script>
+        <p>Renan Queiroz &copy; 2025</p>
+    </footer>
 
-          let social = document.querySelector(".social");
-        social.addEventListener("mouseenter",) () => {
-          social.style.transformX = "100px";
-        }
-          
 
-        
-       
-        </script>
-    </body>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-
+    <link href="//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+</body>
 
 </html>
--- projetos/projetos.php
+++ projetos/projetos.php
@@ -45,9 +45,28 @@ $projetos = [
         <h1 class="title">
             Projetos
         </h1>
-        <a class="voltar" href="../index.php">
-            <span><img src="../imgs/icons8-back-38.png" alt=""></span>
+       
+        <a href="../index.php">
+            <button class="button">
+              <div class="button-box">
+                <span class="button-elem">
+                  <svg viewBox="0 0 46 40" xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M46 20.038c0-.7-.3-1.5-.8-2.1l-16-17c-1.1-1-3.2-1.4-4.4-.3-1.2 1.1-1.2 3.3 0 4.4l11.3 11.9H3c-1.7 0-3 1.3-3 3s1.3 3 3 3h33.1l-11.3 11.9c-1 1-1.2 3.3 0 4.4 1.2 1.1 3.3.8 4.4-.3l16-17c.5-.5.8-1.1.8-1.9z"
+            ></path>
+                  </svg>
+                </span>
+                <span class="button-elem">
+                  <svg viewBox="0 0 46 40">
+            <path
+              d="M46 20.038c0-.7-.3-1.5-.8-2.1l-16-17c-1.1-1-3.2-1.4-4.4-.3-1.2 1.1-1.2 3.3 0 4.4l11.3 11.9H3c-1.7 0-3 1.3-3 3s1.3 3 3 3h33.1l-11.3 11.9c-1 1-1.2 3.3 0 4.4 1.2 1.1 3.3.8 4.4-.3l16-17c.5-.5.8-1.1.8-1.9z"
+            ></path>
+                  </svg>
+                </span>
+              </div>
+            </button>
         </a>
+    
         <div class="container">
             <?php
             foreach ($projetos as $projeto) {
@@ -72,6 +91,9 @@ $projetos = [
             ?>
         </div>
     </main>
+    <footer>
+      <p>Renan Queiroz &copy; 2025</p>
+    </footer>
 </body>
 
 </html>
