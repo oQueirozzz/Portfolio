5bd376e5e42f0c2ba1d55b1c5ab21cb3f088eb50
oQueirozzz <renanqsrodrigues@gmail.com>
2025-01-29 11:50:40 -0300

    tradução feita

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="neon/neon.css">
     <link rel="stylesheet" href="css/global.css">
     <link rel="stylesheet" href="css/home.css">
     <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap" rel="stylesheet">
@@ -12,47 +13,49 @@
 
 
 <body>
-    <header>
-        <nav class="social">
-            <ul>
-                <li><a href="https://www.linkedin.com/in/renan-queiroz-a2a844338/" target="_blank" >Linkedin <img src="imgs/367593_linkedin_business_network_social_icon.png" alt=""></a></li>
-                <li><a href="https://www.instagram.com/oqueiroz._/" target="_blank">Instagram <img src="imgs/1161953_instagram_icon.png" alt=""></a></li>
-                <li><a href="https://github.com/oQueirozzz" target="_blank">GitHub <img src="imgs/1378045_circle_git_github_hovytech_media_icon.png" alt=""></a></li>
-                <li><a href="https://api.whatsapp.com/send?phone=5511966300691&text=Ol%C3%A1%20vim%20pelo%20site%20e%20gostaria%20de%20saber%20mais%20sobre%20seus%20servi%C3%A7os!" target="_blank">Whatsapp <img src="imgs/5305166_call_chat_mobile_whatsapp_whatsapp logo_icon.png" alt=""> </a></li>
-                <li><a href="english/home.php">Português <img src="imgs/brazil-flag-black-and-white-country-national-vector-31611775.jpg" alt=""></a></li>
-            
-            </ul>
-        </nav>
-
-
-    </header>
-    <main>
-        <div class="container">
-            <div class="all">
-                
-                <div class="left" onclick="window.location.href='sobre-mim/sobre.php';"> 
-                    <div class="text">Sobre mim</div>
+    <div id="app">
+        <header>
+            <nav class="social">
+                <ul>
+                    <li><a href="https://www.linkedin.com/in/renan-queiroz-a2a844338/" target="_blank" >Linkedin <img src="imgs/367593_linkedin_business_network_social_icon.png" alt=""></a></li>
+                    <li><a href="https://www.instagram.com/oqueiroz._/" target="_blank">Instagram <img src="imgs/1161953_instagram_icon.png" alt=""></a></li>
+                    <li><a href="https://github.com/oQueirozzz" target="_blank">GitHub <img src="imgs/1378045_circle_git_github_hovytech_media_icon.png" alt=""></a></li>
+                    <li><a href="https://api.whatsapp.com/send?phone=5511966300691&text=Ol%C3%A1%20vim%20pelo%20site%20e%20gostaria%20de%20saber%20mais%20sobre%20seus%20servi%C3%A7os!" target="_blank">Whatsapp <img src="imgs/5305166_call_chat_mobile_whatsapp_whatsapp logo_icon.png" alt=""> </a></li>
+                    <li><a href="english/home.php">Português <img src="imgs/brazil-flag-black-and-white-country-national-vector-31611775.jpg" alt=""></a></li>
+        
+                </ul>
+            </nav>
+        </header>
+        <main>
+            <div class="container">
+                <div class="all">
+        
+                    <div class="left" onclick="window.location.href='sobre-mim/sobre.php';">
+                        <div class="text">Sobre mim</div>
+                    </div>
+                    <div class="center" onclick="window.location.href='projetos/projetos.php';">
+                        <a href="projetos/projetos.php"></a>
+                        <div class="explainer"style="position: absolute; top: 0; left: 0;">
+                    </div>
+                        <div class="text">Meus Projetos</div>
+                    </div>
+                    <div class="right"> <a href="habilidades/habilidades.php"></a>
+                        <div class="text">Minhas Habilidades</div>
+                    </div>
+        
                 </div>
-                <div class="center" onclick="window.location.href='projetos/projetos.php';">
-                    <a href="projetos/projetos.php"></a>
-                    <div class="explainer"style="position: absolute; top: 0; left: 0;">
-                </div>
-                    <div class="text">Meus Projetos</div>
-                </div>
-                <div class="right"> <a href="habilidades/habilidades.php"></a>
-                    <div class="text">Minhas Habilidades</div>
-                </div>
-            
             </div>
-        </div>
-    </main>
-    <footer>
-        <p>Renan Queiroz &copy; 2025</p>
-    </footer>
+        </main>
+        <footer>
+            <p>Renan Queiroz &copy; 2025</p>
+        </footer>
+    </div>
    
 
 
     <link href="//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+    <script type="module" src="neon/neon.js"></script>      
+
 </body>
 
 </html>
